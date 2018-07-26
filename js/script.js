05c27106ce18712139f66d97b62df59dfe80a5a4
geraldokutar <geraldpilk@gmail.com>
2018-07-26 15:56:55 +0300

    Add the rle to every button

--- js/script.js
+++ js/script.js
@@ -9,9 +9,13 @@ $(document).ready(function(){
   $("button#goodbye").click(function(){
     $("ul#user").prepend("<li>Goodbye!</li>")
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>")
+    $("li").click(function(){
+      alert("h1");
   });
   $("button#stop").click(function(){
     $("ul#user").prepend("<li>Stop copying me!</li>")
     $("ul#webpage").prepend("<li>Padon me. I meant no offence</li>")
+    $("li").click(function(){
+      alert("h1");
   })
 })
