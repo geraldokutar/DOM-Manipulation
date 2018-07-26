43e969653a7ec8c440c6e5a7ef363f4f872c6944
geraldokutar <geraldpilk@gmail.com>
2018-07-26 16:00:36 +0300

    Removing the rule in the other buttons

--- js/script.js
+++ js/script.js
@@ -3,19 +3,15 @@ $(document).ready(function(){
     $("ul#user").prepend("<li>Hello!</li>");
     $("ul#webpage").prepend("<li>Why hello there!</li>")
     $("li").click(function(){
-      alert("h1");
+      alert("hi")
     })
   });
   $("button#goodbye").click(function(){
     $("ul#user").prepend("<li>Goodbye!</li>")
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>")
-    $("li").click(function(){
-      alert("h1");
   });
   $("button#stop").click(function(){
     $("ul#user").prepend("<li>Stop copying me!</li>")
     $("ul#webpage").prepend("<li>Padon me. I meant no offence</li>")
-    $("li").click(function(){
-      alert("h1");
   })
 })
