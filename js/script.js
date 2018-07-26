3040d8a44c960abef41b22f88dffe6b754cbcd8d
geraldokutar <geraldpilk@gmail.com>
2018-07-26 15:17:52 +0300

    Add another ul list in first button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   $("button#hello").click(function(){
     $("ul").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>")
   });
   $("button#goodbye").click(function(){
     $("ul").prepend("<li>Goodbye!</li>")
