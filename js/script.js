09e32e4fc41689356ce9b950ac194197577b06d9
geraldokutar <geraldpilk@gmail.com>
2018-07-26 17:00:48 +0300

    Add js rule so that all the other massages can be deleted

--- js/script.js
+++ js/script.js
@@ -12,9 +12,21 @@ $(document).ready(function() {
   $("button#goodbye").click(function() {
     $("ul#user").prepend("<li>Goodbye!</li>")
     $("ul#webpage").prepend("<li>Goodbye, dear user!</li>")
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
   });
+});
   $("button#stop").click(function() {
     $("ul#user").prepend("<li>Stop copying me!</li>")
     $("ul#webpage").prepend("<li>Padon me. I meant no offence</li>")
+    $("ul#user").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+      $(this).remove();
   })
+})
 })
