52b7356b5a11a02ce9b8d632c7871a82e28976da
geraldokutar <geraldpilk@gmail.com>
2018-07-26 15:11:14 +0300

    Add a javascript rule to make button with id goodbye  output Goodbye

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
   $("button#hello").click(function(){
     $("ul").prepend("<li>Hello!</li>");
-  })
+  });
+  $("button#goodbye").click(function(){
+    $("ul").prepend("<li>Goodbye!</li>")
+  });
+
 })
